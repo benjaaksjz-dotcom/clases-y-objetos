a84dd2c2c19eece99478068fd267f1373024be8d
benjaminn <benjaaksjz@gmail.com>
2025-08-22 16:51:59 -0400

    fix

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices.Marshalling;
+
+public class Estudiante
+{
+
+    public string Nombre { get; set; }
+
+    public string? apellido { get; set; }
+
+    public void Informacion()
+} 
+
+{
+Console.WriteLine($"Bienvenido")
+
+}
